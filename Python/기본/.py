16971d239b835c3263a3d1c151ebbcb5b673499b
kim hyowon <sun4131@gmail.com>
2020-07-21 11:23:20 +0900

    Jsutdoit_2020.07.21

--- Python/기본/.py
+++ Python/기본/.py
@@ -1,2 +1,8 @@
-msg = " 한박스에 사과 24개씩 들어있습니다. 몇개가 필요하신가요? \n"
-box = input(msg)2
+Box = ['사과','포도','자몽','사과','배']
+# print(Box['사과'])
+# Box = '사과'
+# print(Box[0])
+print(Box[2:4])
+
+# print(Box[0].next)
+print(next(Box[0]))
